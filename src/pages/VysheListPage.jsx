90d36204b8c633d6bb8401b696dfcd2e1007f863
Илья Лось <moose@MacBook-Air-Ila.local>
2025-12-24 12:40:16 +0300

    Add sand icon

--- src/pages/VysheListPage.jsx
+++ src/pages/VysheListPage.jsx
@@ -1,15 +1,22 @@
+import iconSand from "../assets/icons/sand.svg";
+import iconIzo from "../assets/icons/izo.svg";
+import iconVocal from "../assets/icons/vocal.svg";
+import iconHands from "../assets/icons/hands.svg";
+import iconActor from "../assets/icons/actor.svg";
+import iconPuppets from "../assets/icons/puppets.svg";
+import iconEnglish from "../assets/icons/english.svg";
 import hintIcon from "../assets/icons/hint.svg";
 import LessonCard from "../components/LessonCard";
 import iconAny from "../assets/illustrations/vyshe.svg"; // временно, потом заменим на реальные иконки
 
 const lessons = [
-  { title: "ИЗО", price: "700 ₽", age: "от 6 лет" },
-  { title: "Вокал", price: "700 ₽", age: "от 6 лет" },
-  { title: "Очумелые ручки", price: "700 ₽", age: "от 6 лет" },
-  { title: "Актерское мастерство", price: "700 ₽", age: "от 6 лет" },
-  { title: "Театр кукол", price: "700 ₽", age: "от 6 лет" },
-  { title: "Английский язык", price: "700 ₽", age: "от 8 лет" },
-  { title: "Песочная анимация", price: "700 ₽", age: "от 6 лет" },
+  { title: "ИЗО", price: "700 ₽", age: "от 6 лет", icon: iconIzo },
+  { title: "Вокал", price: "700 ₽", age: "от 6 лет", icon: iconVocal },
+  { title: "Очумелые ручки", price: "700 ₽", age: "от 6 лет", icon: iconHands },
+  { title: "Актерское мастерство", price: "700 ₽", age: "от 6 лет", icon: iconActor },
+  { title: "Театр кукол", price: "700 ₽", age: "от 6 лет", icon: iconPuppets },
+  { title: "Английский язык", price: "700 ₽", age: "от 8 лет", icon: iconEnglish },
+  { title: "Песочная анимация", price: "700 ₽", age: "от 6 лет", icon: iconSand },
   { title: "Лепка", price: "650 ₽", age: "от 5 лет" },
   { title: "Музыкальный кружок", price: "700 ₽", age: "от 4 лет" },
   { title: "Шахматы", price: "800 ₽", age: "от 7 лет" },
@@ -47,13 +54,13 @@ export default function VysheListPage({ onHelp, onOpenLesson }) {
       <div className="vysheList">
         {lessons.map((l) => (
           <LessonCard
-            key={l.title}
-            iconSrc={iconAny}
-            title={l.title}
-            price={l.price}
-            age={l.age}
-            onClick={() => onOpenLesson(l)} // важно: передаём lesson в App.jsx
-          />
+          key={l.title}
+          iconSrc={l.icon || iconAny}
+          title={l.title}
+          price={l.price}
+          age={l.age}
+          onClick={() => onOpenLesson(l)}
+        />
         ))}
       </div>
     </div>
